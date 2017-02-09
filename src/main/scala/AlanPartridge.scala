36388a5cefb62815bd5bab0f5e3b89f0d6a5d004
Olly Shaw <ollyjshaw@gmail.com>
2017-02-09 11:56:41 +0000

    A solution from the session

--- src/main/scala/AlanPartridge.scala
+++ src/main/scala/AlanPartridge.scala
@@ -1,35 +1,16 @@
 object AlanPartridge {
+
   val alanisms = List("Pear Tree", "Chat","Dan","Toblerone","Lynn","Alpha Papa","Nomad")
 
   def findWords(words: List[String]) = {
-
-    var c = 0;
-    var numberFound=0
-    while (c < words.length){
-
-      var tempCounter=0
-      var found=false
-
-      while(tempCounter<=alanisms.length-1 && found==false) {
-        println(tempCounter + words(c))
-        if (words(c) == alanisms(tempCounter)) {
-          tempCounter=0
-          found = true
-          numberFound = numberFound + 1
-        }
-        else {
-          tempCounter = tempCounter + 1
-        }
-      }
-      c=c+1
-    }
-
-    if (numberFound>0)
-      "Mines a pint" ++ List.fill(numberFound)("!").mkString
-    else
-      "I've stood on a spike!"
-
+    val numberFound = words.filter(alanisms.contains(_)).size
+    constructPhrase(numberFound)
   }
 
-
+  private def constructPhrase(termsFound: Int) = {
+    if (termsFound>0)
+      "Mines a pint" ++ List.fill(termsFound)("!").mkString
+    else
+      "I've stood on a spike!"
+  }
 }
--- src/test/scala/AlanPartridgeSpec.scala
+++ src/test/scala/AlanPartridgeSpec.scala
@@ -4,13 +4,24 @@ import org.scalatest.{FlatSpec, Matchers, WordSpecLike, _}
 class AlanPartridgeSpec extends WordSpecLike with Matchers {
   "Calling findWords" should {
 
+    "return I've stood on a spike where an list with no words are passed" in {
+      AlanPartridge.findWords(List()) shouldBe "I've stood on a spike!"
+    }
+
+    "return I've stood on a spike where a mixed list with no is passed" in {
+      AlanPartridge.findWords(List("Cat","Dog", "Lynn", "Lard")) shouldBe "Mines a pint!"
+    }
+
     "return I've stood on a spike where an list with no matching words is passed" in {
       AlanPartridge.findWords(List("Cat","Dog","Lard")) shouldBe "I've stood on a spike!"
     }
 
-
     "return mines a pint! with one ! where an list with one matching words is passed" in {
       AlanPartridge.findWords(List("Nomad")) shouldBe "Mines a pint"+ "!"
+    }
+
+    "return mines a pint! with one ! where an list with three matching words are passed" in {
+      AlanPartridge.findWords(List("Nomad", "Lynn", "Dan")) shouldBe "Mines a pint"+ "!!!"
     }
 
   }
